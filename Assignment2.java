711c207d926b76a02ad70a585a9128453371efa3
John Kevin Abgao <111432788+Curious-person@users.noreply.github.com>
2022-10-03 11:23:55 +0800

    Create Assignment2.java

---
+++ Assignment2.java
@@ -0,0 +1,34 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //FOR LOOP
+        for (int i = 5; i != 0; i--) {
+           
+            
+            if (i == 4) {
+                System.out.println("break");
+                break;
+            }
+             System.out.println("John Kevin");
+        }
+        
+        //WHILE LOOP
+        int  nicknameCount = 5;
+        while ( nicknameCount != 0) {
+            
+            nicknameCount--;
+            if (nicknameCount == 3) {
+                System.out.println("Go ahead i will skip this third loop. i will continue");
+                continue;
+            }
+            System.out.println("kej");
+        } 
+        
+        //DO WHILE LOOP
+        int nameCount = 5;
+        do {
+            System.out.println("abgao");
+            nameCount--;
+        } while (nameCount != 0);
+    }
+    
+    }
